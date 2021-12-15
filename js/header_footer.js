0cc99cdc1022e0f6ff1f4b570065a13e7324b19c
dusuna <dusunax@gmail.com>
2021-12-15 20:33:18 +0900

    211215 header끝

---
+++ js/header_footer.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let btn_nav=document.querySelector(".icon_nav")
+    let header=document.querySelector("header")
+    let nav=document.querySelector("nav")
+    let s_top=document.querySelector('html').scrollTop;
+    let h_bot_oTop=document.querySelector('.h_bot').offsetTop;
+
+    btn_nav.addEventListener("click", function(){
+        header.classList.toggle('nav_active')
+        nav.classList.toggle('active')
+    })
+    document.addEventListener("scroll", function(){
+        s_top=document.querySelector('html').scrollTop;
+        if(s_top >= h_bot_oTop){
+            header.classList.add('float')
+        }
+        else {
+            header.classList.remove('float')
+        }
+    })
+});
